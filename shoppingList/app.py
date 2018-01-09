d99e2dc8d07d0675743726f5a596080e189508b0
J C <candyer@users.noreply.github.com>
2018-01-09 23:37:38 +0100

    create a shopping list with database

--- shoppingList/app.py
+++ shoppingList/app.py
@@ -3,6 +3,7 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
 	return 'welcome'
@@ -11,38 +12,42 @@ def hello():
 def user():
 	return render_template('user.html')
 
-@app.route('/list_food', methods=['GET'])
+@app.route('/list_items', methods=['GET'])
 def list_food():
-	return jsonify(query_entry('food'))
+	return jsonify(query_entry())
 
-@app.route('/add', methods=['POST'])
-def add():
+@app.route("/add", methods=['POST'])
+def add_item():
 	data = request.get_json()
-	image = request.files['inputImage']
-	return file.read()
 	if data:
-		push_item_to_inventory(data['item_name'], data['item_image'])
+		insert_entry(data['item_name'], data['item_image'])
 		return '{status: "ok"}'
 	return '{status: "fail"}'
 
-def create_table(category):
-	conn = sqlite3.connect('shopping.db')
-	c = conn.cursor()
-	c.execute('CREATE TABLE IF NOT EXISTS {tb_name} (id INTEGER PRIMARY KEY, name TEXT, image BLOB)' .format(tb_name=category))
+@app.route("/list_items", methods=['GET'])
+def list_items():
+	return jsonify(query_entry())
 
-def insert_entry(category, item_name, item_image):
+
+def create_table():
 	conn = sqlite3.connect('shopping.db')
 	c = conn.cursor()
-	c.execute('INSERT INTO {tb_name} (name, image) VALUES ({n}, {i})'.\
-		format(tb_name=category, n=item_name, i=item_image))
+	c.execute('CREATE TABLE IF NOT EXISTS list (id INTEGER PRIMARY KEY, name TEXT, image TEXT);')
+
+# create_table()
+
+def insert_entry(item_name, item_image):
+	conn = sqlite3.connect('shopping.db')
+	c = conn.cursor()
+	c.execute('INSERT INTO list (name, image) VALUES (?, ?);',(item_name, item_image))
 	conn.commit()
 	c.close()
 	conn.close()
 
-def query_entry(category):
+def query_entry():
 	conn = sqlite3.connect('shopping.db')
 	c = conn.cursor()
-	c.execute('SELECT name, image FROM {tb_name} order by id'.format(tb_name=category))
+	c.execute('SELECT name, image FROM list order by id')
 	x = c.fetchall()
 	conn.commit()
 	c.close()
@@ -51,3 +56,6 @@ def query_entry(category):
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+
