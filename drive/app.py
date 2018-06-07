d494c466dd0466439f18d41f5c7ffb32997d0683
cc <candyer@users.noreply.github.com>
2018-06-07 17:34:09 +0200

    add login page

--- drive/app.py
+++ drive/app.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, flash, render_template, url_for 
-
+from flask import Flask, request, flash, render_template, url_for, redirect
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
-
 import sqlite3
 import os
 
@@ -9,19 +7,21 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24) # flash requires this
 
 
+
 @app.route('/')
 def home():
 	return 'welcome'
 
 
+
 class RegistrationForm(Form):
-	username = StringField('Username', [validators.Length(min=4, max=25)])
-	password = PasswordField('New Password', [
+	username = StringField('', [validators.Length(min=4, max=25)], render_kw={"placeholder": "Username"})
+	password = PasswordField('', [
 		validators.DataRequired(),
 		validators.EqualTo('confirm', message='Passwords must match')
-	])
-	confirm = PasswordField('Repeat Password')
-	accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+		], 
+		render_kw={"placeholder": "Password"})
+	confirm = PasswordField('', render_kw={"placeholder": "Repeat Password"})
 
 
 
@@ -31,7 +31,22 @@ def register():
 	if request.method == 'POST' and form.validate():
 		insert_user(form.username.data, form.password.data)
 		flash('Thanks for registering')
+		return redirect(url_for('login'))
 	return render_template('register.html', form=form)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if check_username(request.form['username']) == None:
+			error = 'Username does not exist, please try again'
+		elif request.form['password'] != check_password(request.form['username']):
+			error = 'Password does not match, pleae try again'
+		else:
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
 
 
 
@@ -45,6 +60,7 @@ def create_table():
 # create_table()
 
 
+
 def insert_user(username, password):
 	conn = sqlite3.connect('drive.db')
 	c = conn.cursor()
@@ -52,6 +68,32 @@ def insert_user(username, password):
 	conn.commit()
 	c.close()
 	conn.close()
+
+
+
+def check_username(username):
+	conn = sqlite3.connect('drive.db')
+	c = conn.cursor()
+	c.execute("SELECT name FROM users where name = ?", (username,))
+	data = c.fetchone()	
+	conn.commit()
+	c.close()
+	conn.close()
+	return data
+
+
+
+def check_password(username):
+	conn = sqlite3.connect('drive.db')
+	c = conn.cursor()
+	c.execute("SELECT password FROM users where name = ?", (username,))
+	data = c.fetchone()	
+	conn.commit()
+	c.close()
+	conn.close()
+	return data[0]
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
