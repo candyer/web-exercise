83ff77a36148ccf16d61e1101a30a7d5a8f60a21
c <candyer@users.noreply.github.com>
2018-06-18 17:10:03 +0200

    remove finished tasks

--- todoList/app.py
+++ todoList/app.py
@@ -4,6 +4,7 @@ import sqlite3
 app = Flask(__name__)
 
 
+
 @app.route('/',  methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
@@ -11,31 +12,44 @@ def home():
 		if task:
 			insert_task(task)
 
+
+	elif request.method == 'GET':
+		for id in request.args['task']:
+			remove_tasks(id)
+
 	tasks = []
 	for id, task in get_tasks():
-		line = "<p><input type='checkbox' name= %d > %s </p>" % (id, escape_string(task))
+		line = "<p><input type='checkbox' name='task' value= %d> %s </p>" % (id, escape_string(task))
 		tasks.append(line)
-	tasks = ''.join(tasks)		
+	tasks = ''.join(tasks)	
+
 
 	return ''' 
-
 		<!DOCTYPE html>
 		<html>
 			<body>
 				<h1>To do List</h1>
 				<form action="" method="POST" >
 					<input type='text' name='task' placeholder="add a task">
-					<button>Add</button>
+					<input type="submit" value="Add">
 				</form>
 
-				<form>
+				<form action="" method="GET">
 					%s
+					<input type="submit" value="Finish">
 				</form>
-				<button>Finish</button>
+				
 			</body>
 		</html>
-
 	''' % (tasks)
+
+
+
+def escape_string(s):
+	'''
+	escape dangerous characters(<, >, ', "), there are more but this is just an example
+	'''
+	return s.replace('<', '&lt;').replace('>', '&gt;').replace("'", '&apos;').replace('"', '&quot;')
 
 
 
@@ -60,23 +74,25 @@ def insert_task(task):
 
 
 
-def escape_string(s):
-	'''
-	escape dangerous characters(<, >, ', "), there are more but this is just an example
-	'''
-	return s.replace('<', '&lt;').replace('>', '&gt;').replace("'", '&apos;').replace('"', '&quot;')
-
-
-
 def get_tasks():
 	conn = sqlite3.connect('todolist.db')
 	c = conn.cursor()	
-	c.execute("SELECT id, task FROM tasks ")
+	c.execute("SELECT id, task FROM tasks")
 	tasks = c.fetchall()
 	conn.commit()
 	c.close()
 	conn.close()
 	return [[id, task] for id, task in tasks]
+
+
+
+def remove_tasks(id):
+	conn = sqlite3.connect('todolist.db')
+	c = conn.cursor()
+	c.execute('DELETE FROM tasks WHERE id = {}'.format(id))
+	conn.commit()
+	c.close()
+	conn.close()
 
 
 
