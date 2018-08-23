bfdf8af4f75c6e9f374bc4711a32a8058584667b
c <candyer@users.noreply.github.com>
2018-08-23 15:05:47 +0200

    Making an Interactive Component

--- tic-tac-toe/my-app/src/index.js
+++ tic-tac-toe/my-app/src/index.js
@@ -4,14 +4,24 @@ import './index.css';
 
 
 class Square extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: null,
+		};
+	}
+
 	render() {
 		return (
-			<button className="square">
-				{this.props.value}
+			<button className="square" 
+					onClick={()=> this.setState({value: 'X'})}
+			>
+				{this.state.value}
 			</button>
 		);
 	}
 }
+
 
 class Board extends React.Component {
 	renderSquare(i) {
